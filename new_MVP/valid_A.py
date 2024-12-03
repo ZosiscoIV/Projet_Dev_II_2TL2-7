611719523db5342d4e1da039c484f35f71732d18
Antoine <antoinebontems05@gmail.com>
2024-12-03 12:50:06 +0100

    Début Test Unitaires

---
+++ new_MVP/valid_A.py
@@ -0,0 +1,23 @@
+import json
+from datetime import datetime
+
+from table import Table
+from commande import Commande
+from plat import Plat
+from client import Client
+from reservation import Reservation
+
+
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_un_truc(self):
+        cmd1 = Commande('P')
+        cmd2 = Commande('C')
+        self.assertEqual(cmd1.etat_commande, 'P')
+        self.assertEqual(cmd2.etat_commande, 'C')
+
+
+if __name__ == '__main__':
+    unittest.main()
