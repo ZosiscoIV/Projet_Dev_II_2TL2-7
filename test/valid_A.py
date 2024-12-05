630cbd1b738429bb0c546ee9d126505a59eca1cb
Antoine <antoinebontems05@gmail.com>
2024-12-05 21:06:11 +0100

    Validation A

--- test/valid_A.py
+++ test/valid_A.py
@@ -12,7 +12,7 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def test_init_commande(self):
+    def test_init_commande_et_increment_compteur(self):
 
         Commande.compteur = 1
 
@@ -59,6 +59,86 @@ class Test(unittest.TestCase):
             cmd2.etat_commande = ""
         with self.assertRaises(ValueError):
             cmd2.etat_commande = None
+
+    def test_ajouter_plat(self):
+
+        plat1 = Plat("Bolo", ["Pate", "Bolo", "Persil"], 12.5)
+        plat2 = Plat("Pizza", ["Pate","Tomate", "Fromage", "Jambon"], 9.7)
+        plat3 = Plat("Steak", ["Steak", "Frites", "Salade", "Archiduc"], 27.1)
+
+
+        cmd3 = Commande()
+        self.assertEqual(cmd3.plats, [])
+        cmd3.ajouter_plat(plat1)
+        self.assertEqual(cmd3.plats[0], plat1)
+
+        with self.assertRaises(ValueError):
+            cmd3.ajouter_plat(42)
+
+
+        cmd4 = Commande()
+
+        cmd4.ajouter_plat(plat1)
+        cmd4.ajouter_plat(plat2)
+
+        cmd4.ajouter_plat(plat3)
+        self.assertEqual(cmd4.plats[2], plat3)
+
+        with self.assertRaises(ValueError):
+            cmd4.ajouter_plat(42)
+
+
+
+    def test_retirer_plat(self):
+
+        plat1 = Plat("Bolo", ["Pate", "Bolo", "Persil"], 12.5)
+        plat2 = Plat("Pizza", ["Pate", "Tomate", "Fromage", "Jambon"], 9.7)
+        plat3 = Plat("Steak", ["Steak", "Frites", "Salade", "Archiduc"], 27.1)
+
+        cmd5 = Commande("C")
+        cmd5.ajouter_plat(plat1)
+        self.assertEqual(cmd5.plats[0], plat1)
+
+        with self.assertRaises(IndexError):
+            cmd5.retirer_plat(19)
+        with self.assertRaises(IndexError):
+            cmd5.retirer_plat(plat1)
+
+        cmd5.retirer_plat(0)
+        self.assertEqual(cmd5.plats, [])
+
+
+        cmd6 = Commande("C")
+
+        with self.assertRaises(IndexError):
+            cmd6.retirer_plat(0)
+
+        cmd6.ajouter_plat(plat1)
+        cmd6.ajouter_plat(plat2)
+        cmd6.ajouter_plat(plat3)
+
+        self.assertEqual(cmd6.plats[0], plat1)
+        self.assertEqual(cmd6.plats[1], plat2)
+        self.assertEqual(cmd6.plats[2], plat3)
+
+        cmd6.retirer_plat(1)
+
+        self.assertEqual(cmd6.plats[0], plat1)
+        self.assertEqual(cmd6.plats[1], plat3)
+
+        cmd6.ajouter_plat(plat3)
+        self.assertEqual(cmd6.plats[2], plat3)
+
+        cmd6.retirer_plat(1)
+        self.assertEqual(cmd6.plats[1], plat3)
+        with self.assertRaises(IndexError):
+            cmd6.retirer_plat(2)
+
+        with self.assertRaises(IndexError):
+            cmd6.retirer_plat(-1)
+
+
+
 
 
 if __name__ == '__main__':
